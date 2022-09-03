bab85ac07d7dec565051135d32ea5fa4f3c06c31
salah <sonyexperiasola29@gmail.com>
2022-09-03 19:26:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
             <Button variant={'contained'} onClick={() => alert('hello heroku')}>
                 hello heroku
             </Button>
+            <Button variant={'contained'} onClick={() => alert('hello heroku')}>
+                hello heroku
+            </Button>
         </Stack>
     </div>
   );
