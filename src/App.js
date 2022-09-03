480c4cc4f116934e5dd9f478d264b511febfe40d
salah <sonyexperiasola29@gmail.com>
2022-09-03 18:37:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import getLanguages from "./test";
+import {Button, Paper, Stack} from "@mui/material";
 
 function App() {
+  const getData = getLanguages
+  const data = getData() ;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+        <Stack alignItems={'center'} padding={20}>
+            <Button variant={'contained'} onClick={() => alert('hello heroku')}>
+                hello heroku
+            </Button>
+        </Stack>
     </div>
   );
 }
